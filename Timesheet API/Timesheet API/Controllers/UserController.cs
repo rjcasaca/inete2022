7b324923deb93e0361b7945840744c70eae9137b
GabrielPacheco2003 <gpacheco182003@gmail.com>
2022-05-02 11:16:26 +0100

    first commit

---
+++ Timesheet API/Timesheet API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Timesheet_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Timesheet API/Timesheet API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Timesheet_API.Models
+{
+    [Table(name:"User")]
+    public class User
+    {
+        [Key, MaxLength(254)]
+        public string email { get; set; }
+        [Required, MaxLength(250)]
+        public string nome { get; set; }
+    }
+}
