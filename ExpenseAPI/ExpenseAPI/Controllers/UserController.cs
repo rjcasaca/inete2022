2b7e96bc6a5d1d57730812232ab905d96912358a
Im-Lucas12 <lucasdanielsilvadecarvalho@gmail.com>
2022-05-03 09:13:00 +0100

    ExepenseAPI

---
+++ ExpenseAPI/ExpenseAPI/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        { 
+        return Ok();
+        
+        
+        }
+
+
+        [HttpPost]
+        public IActionResult Post() { return Ok(); }
+        [HttpPut]
+        public IActionResult Pt() { return Ok(); }
+        [HttpDelete]
+        public IActionResult Delete() { return Ok(); }
+
+    }
+}
---
+++ ExpenseAPI/ExpenseAPI/Migrations/20220502151102_firstMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseAPI.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Email);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
