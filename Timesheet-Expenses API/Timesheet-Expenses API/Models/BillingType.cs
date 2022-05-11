15039eae88d6fcde13be20fdeb8ee24f651e7f92
GabrielPacheco2003 <gpacheco182003@gmail.com>
2022-05-11 15:10:12 +0100

    6th Commit

---
+++ Timesheet-Expenses API/Timesheet-Expenses API/Models/BillingType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Timesheet_Expenses_API.Models
+{
+    [Table(name:"Billing Type")]
+    public class BillingType
+    {
+        [Key]
+        public int BillingType_Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Type { get; set; }
+
+        //Navigation Properties
+        public List<Worklog> Worklog { get; set; }
+    }
+}
